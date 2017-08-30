bac122798af177a581c71bc03a4ce33d849f10d6
Jon Kimble <jonathanbkimble@gmail.com>
2017-08-30 15:12:53 -0700

    Refactored to cleaner solution

--- ruby/grade-school/grade_school.rb
+++ ruby/grade-school/grade_school.rb
@@ -1,46 +1,27 @@
-require 'pry'
-
 class School
   def initialize
-    @students = {}
+    @registry = Hash.new([])
   end
 
   def students(grade)
-    names = @students[grade] || []
-    names.sort
+    @registry[grade]
   end
 
   def add(name, grade)
-    @students[grade] ||= []
-    @students[grade] << name
+    grade_registry = @registry[grade]
+    if grade_registry == []
+      @registry[grade] = Array.new(0, grade_registry).push(name)
+    else
+      @registry[grade] = Array.new(1, grade_registry).push(name).flatten.sort
+    end
   end
 
   def students_by_grade
-    registry = []
-    return registry if @students.empty?
-
-    @students.keys.sort.each do |grade|
-      students = {}
-      students[:grade] = grade
-      students[:students] = @students[grade].sort
-      registry.push(students)
-    end
-    return registry
+    @registry.sort.map {|grade, students| {grade: grade, students: students}}
   end
+
 end
 
-# quality solution from cmccandless, for study later!
-# class School
-#   def initialize
-#     @roster = Hash.new([])
-#   end
-#   def students(grade)
-#     @roster[grade]
-#   end
-#   def add(name, grade)
-#     @roster[grade] = Array.new(@roster[grade]).push(name).sort
-#   end
-#   def students_by_grade
-#     @roster.sort.collect { |g,s| {:grade => g, :students => s } }
-#   end
-# end
+def test_bookkeeping
+  assert_equal 3, BookKeeping::VERSION
+end
