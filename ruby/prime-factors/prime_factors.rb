3a912b867725a740f22389a553e6174a2ff9f675
Jon Kimble <jonathanbkimble@gmail.com>
2017-09-17 10:28:35 -0700

    refactored solution

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,18 +1,16 @@
 require 'prime'
-require 'pry'
 
 class PrimeFactors
+
   def self.for(dividend)
     result = []
       Prime.each do |prime|
         break if dividend == 1
-        if (dividend % prime) == 0
-          until (dividend % prime) != 0
-            dividend /= prime
-            result << prime
-          end
-          end
+        until (dividend % prime) != 0
+          dividend /= prime
+          result << prime
         end
+      end
     result
   end
 
