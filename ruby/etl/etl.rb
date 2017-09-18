122b2039ccb8dfd7d30f8455ff57edc9784afe59
Jon Kimble <jonathanbkimble@gmail.com>
2017-09-18 10:24:08 -0700

    Minor cosmetic change for readability

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -3,13 +3,11 @@ class ETL
   def self.transform(legacy_score_system)
     new_score_system = Hash.new
     legacy_score_system.each do |score, letters|
-      letters.each do |letter|
-        new_score_system[letter.downcase] = score
-      end
+      letters.each { |letter| new_score_system[letter.downcase] = score }
     end
     new_score_system
   end
-  
+
 end
 
 module BookKeeping
